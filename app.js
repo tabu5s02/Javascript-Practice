977b82859c5577f099d66a5b3694c63a2a7bd086
Habeeba Thabasum Shaik <tabu592002@gmail.com>
2021-09-03 12:59:53 +0530

    About console

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Hey this is my first piece of JS code!!! YAY')
+console.log(123456789)
+
+console.log({name: "Tabu"})
+
+console.log(true)
+console.log(false)
+
+console.log(false, 234234, 'string', { name:"Tabu"})
+
+console.warn("this is a warning")
+
+console.error("This is an error")
